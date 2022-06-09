52a185dd2687148f18bac5d5670c879aff30c53e
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-06-09 11:04:36 +0300

    ииииии

---
+++ WebApplication1/Data/ToDoItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace WebApplication1.Data
+{
+    public class ToDoItem
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id;
+
+        public string Name { get; set; }
+        public bool IsDone { get; }
+
+        public static void AddStudentToDatabase(ToDoItem newToDo)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ToDoList");
+            var collection = database.GetCollection<ToDoItem>("ToDoItem");
+            collection.InsertOne(newToDo);
+        }
+    }
+}
