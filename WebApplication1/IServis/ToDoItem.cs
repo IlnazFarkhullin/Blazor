06e9f984b14532a2e112590f709eeb201d25c192
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-06-12 12:54:03 +0300

    Работа с MongoDB

---
+++ WebApplication1/IServis/ToDoItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+
+namespace WebApplication1.Data
+{
+    public class ToDoItem
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id;
+
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+
+       
+    //public static  List<ToDoItem> ShowToItem()
+    //{
+    //    var todo = new MongoClient("mongodb://localhost");
+    //    var database = todo.GetDatabase("ToDoList");
+    //    var collection = database.GetCollection<ToDoItem>("List");
+    //    return collection.Find(x => true).ToList();
+    //}
+    }
+
+    //public static List<students_class> ShowStudents()
+    //{
+    //    var stud = new MongoClient("mongodb://localhost"); // подключение
+    //    var database = stud.GetDatabase("guide"); // бд
+    //    var collection = database.GetCollection<BD_class.students_class>("Students"); // коллекция внутри БД
+    //    return collection.Find(x => true).ToList(); // возвращение всех данных из коллекции
+    //}
+}
